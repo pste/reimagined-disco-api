ddfa5d58184664c3b5488d5a42f8132fa7533f62
pste <pirraste@gmail.com>
2025-04-15 16:00:51 +0200

    [db] explicit connection params

--- src/dbmodels/dbpool.js
+++ src/dbmodels/dbpool.js
@@ -1,9 +1,15 @@
 const pg = require('pg');
 const { Pool } = pg;
 
-const pool = new Pool({
+const config = {
     max: 20,
     idleTimeoutMillis: 20000,
-})
+    user: process.env.PGUSER,
+    password: process.env.PGPASSWORD,
+    host: process.env.PGHOST,
+    port: process.env.PGPORT,
+    database: process.env.PGDATABASE,
+}
+const pool = new Pool(config)
 
 module.exports = pool;
