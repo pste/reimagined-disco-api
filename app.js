8f7858c88bea10b8d765bf296acad517bc7ef1b4
pste <pirraste@gmail.com>
2025-07-24 14:12:11 +0200

    [scan] scan on boot

--- app.js
+++ app.js
@@ -2,8 +2,10 @@ const server = require('./src/server');
 const filescan = require('./src/filescan');
 
 async function start() {
-    await filescan.installJob();
     await server.run();
+    await filescan.installJob();
+    // run on boot
+    filescan.fastscan();
 }
 
 start()
