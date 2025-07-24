5fe0954d4369dad1dbe2891f304de2952a18b721
pste <pirraste@gmail.com>
2025-07-24 13:33:13 +0200

    [params] added params module

---
+++ src/parameters.js
@@ -0,0 +1,25 @@
+const logger = require('./logger');
+const db = require('./db');
+
+//
+async function baseDir() {
+    const sources = await db.getSources();
+    let basedir = '/home/steo/DEV/reimagined-disco-api/private'; // './private'; // TODO
+    if (sources.length > 0) {
+        basedir = sources[0].path; // TODO reading the 1st one ?
+    }
+    logger.info(`Obtaining base folder: ${basedir} ...`)
+    return basedir;
+}
+
+//
+async function scanJobDefinition() {
+    // TODO on DB
+    return '0 23 * * 1,5'; // At 23:00 on Monday and Friday.
+}
+
+//
+module.export = {
+    baseDir,
+    scanJobDefinition
+}
