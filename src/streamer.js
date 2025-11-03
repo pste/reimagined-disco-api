7fa9802a8a38aecb8048bf9327ddfa24f34a0470
pste <pirraste@gmail.com>
2025-11-03 11:39:07 +0100

    [streamer] added chunkfile method

--- src/streamer.js
+++ src/streamer.js
@@ -3,9 +3,9 @@ const logger = require('./logger');
 
 function streamFile( request, reply, filepath ) {
     const audioSize = fs.statSync(filepath).size;
-    logger.info("STREAM: Requested size:" + audioSize);
+    logger.info(`STREAM: Requested size: ${audioSize}`);
     const range = request.range(audioSize);
-    logger.info(range, "STREAM: Requested range");
+    logger.info(range, `STREAM: Requested range.`);
     if (!range) {
         const error = new Error('Range Not Satisfiable');
         error.statusCode = 416;
@@ -35,6 +35,21 @@ function streamFile( request, reply, filepath ) {
     return stream;
 }
 
+async function chunkFile(request, reply, filepath) {
+    const stream = fs.createReadStream(filepath, { highWaterMark: 1 * 1024/*, encoding: 'utf8'*/ });
+    reply.send(stream); // that should be enough
+    /*
+    stream.on('data', function(chunk) {
+        data += chunk;
+        console.log('chunk Data : ')
+        console.log(chunk);// your processing chunk logic will go here
+
+    }).on('end', function() {
+        console.log('done chunking');
+    });*/
+}
+
 module.exports = {
-    streamFile
+    streamFile,
+    chunkFile,
 }
