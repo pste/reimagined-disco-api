2d4ff502212f573befa5b5455112c77879c9d037
pste <pirraste@gmail.com>
2026-05-27 15:29:22 +0200

    [filescan] removed scan, now is in cronjob

--- app.js
+++ app.js
@@ -1,17 +1,7 @@
-const logger = require('./src/logger');
 const server = require('./src/server');
-const filescan = require('./src/filescan');
 
 async function start() {
     await server.run();
-    await filescan.installJob();
-    // run on boot
-    if (process.env.BOOTSCAN === "yes") {
-        await filescan.fastscan();
-    }
-    else {
-        logger.info("*** DEVSERVER MODE ON *** (no scan on boot)")
-    }
 }
 
 start()
